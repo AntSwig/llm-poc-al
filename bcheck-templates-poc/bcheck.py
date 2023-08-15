c4b8444bcc73a69126532fb46245d457f01816e3
Katie Hemus <katie.hemus@portswigger.net>
2023-08-15 16:38:02 +0100

    Load data from two seperate data sources & update dependencies

--- bcheck-templates-poc/bcheck.py
+++ bcheck-templates-poc/bcheck.py
@@ -2,10 +2,14 @@ import logging
 import os
 import shutil
 
+import requests
+import xmltodict
+from bs4 import BeautifulSoup
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import GitLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 
@@ -18,6 +22,39 @@ bcheck_loader = GitLoader(
 bcheck_data = bcheck_loader.load()
 
 (len(bcheck_data))
+
+
+def extract_text_from(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, features="html.parser")
+    text = soup.get_text()
+
+    lines = (line.strip() for line in text.splitlines())
+    return "\n".join(line for line in lines if line)
+
+
+r = requests.get("https://portswigger.net/sitemap.xml")
+xml = r.text
+raw = xmltodict.parse(xml)
+
+pages = []
+for info in raw["urlset"]["url"]:
+    # info example: {'loc': 'https://portswigger.net/burp/documentation...', 'lastmod': '2021-12-28'}
+    url = info["loc"]
+    if (
+        "https://portswigger.net/burp/documentation/scanner/bchecks/bcheck-definition-reference"
+        in url
+    ):
+        pages.append({"text": extract_text_from(url), "source": url})
+print(pages[0])
+
+text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
+docs, metadatas = [], []
+for page in pages:
+    splits = text_splitter.split_text(page["text"])
+    docs.extend(splits)
+    metadatas.extend([{"source": page["source"]}] * len(splits))
+    print(f"Split {page['source']} into {len(splits)} chunks")
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1500,
